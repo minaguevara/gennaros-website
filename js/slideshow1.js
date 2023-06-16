fbc81581d9cf0ba111ff34e53e713bf48bcb21c7
Mina Guevara <107224237+minaguevara@users.noreply.github.com>
2023-06-16 16:04:19 -0500

    Updated Meny

--- js/slideshow1.js
+++ js/slideshow1.js
@@ -2,21 +2,21 @@ var slideIndex = 0;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);
+  showDivs(slideIndex + n);
 }
 
 function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides1");
-
-    if (n >= x.length) {
-        slideIndex = 0;
-      }
-      if (n < 0) {
-        slideIndex = x.length - 1;
-      }
-      for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-      }
-      x[slideIndex].style.display = "block";
+  var i;
+  var x = document.getElementsByClassName("mySlides1");
+  if (n >= x.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = x.length - 1;
+  } else {
+    slideIndex = n;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex].style.display = "block";
 }
